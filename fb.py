a462934c5fe1c74952501674067bd63b6e27cc82
Dan <dansonlinepresence@gmail.com>
2017-07-26 22:19:14 -0400

    (fb) refactor parser

--- fb.py
+++ fb.py
@@ -5,38 +5,48 @@ parser.add_argument('--word-frequency', '--wf', action='store_true', help='count
 parser.add_argument('--word-frequency-user', '--wfu', help='user to get word frequency for, default all users')
 args=parser.parse_args()
 
+from html.parser import HTMLParser
+class MessageParser(HTMLParser):
+	def __init__(self, desired_user):
+		HTMLParser.__init__(self)
+		self.desired_user=desired_user
+		self.stack=[]
+		self.user=None
+		self.meta=None
+
+	def handle_starttag(self, tag, attrs):
+		self.stack.append((tag, attrs))
+
+	def handle_endtag(self, tag):
+		assert tag==self.stack[-1][0]
+		self.stack=self.stack[:-1]
+
+	def handle_data(self, data):
+		if   self.stack[-1]==('span', [('class', 'user')]):
+			self.user=data
+		elif self.stack[-1]==('span', [('class', 'meta')]):
+			self.meta=data
+		elif (self.desired_user and self.user==self.desired_user) or ((not self.desired_user) and self.user):
+			self.handle_message(data)
+
+	def go(self):
+		import os
+		with open(os.path.join(args.path, 'html', 'messages.htm')) as file: self.feed(file.read())
+
 if args.word_frequency:
 	import re
-	from html.parser import HTMLParser
-	class MessageParser(HTMLParser):
-		def __init__(self, desired_user):
-			HTMLParser.__init__(self)
-			self.desired_user=desired_user
-			self.stack=[]
-			self.user=None
-			self.meta=None
+	class WordCountParser(MessageParser):
+		def __init__(self, *args, **kwargs):
+			MessageParser.__init__(self, *args, **kwargs)
 			from collections import defaultdict
 			self.wordcount=defaultdict(int)
 
-		def handle_starttag(self, tag, attrs):
-			self.stack.append((tag, attrs))
+		def handle_message(self, message):
+			for word in message.split():
+				self.wordcount[re.sub(r'[^a-z\-\']', '', word.lower())]+=1
 
-		def handle_endtag(self, tag):
-			assert tag==self.stack[-1][0]
-			self.stack=self.stack[:-1]
-
-		def handle_data(self, data):
-			if   self.stack[-1]==('span', [('class', 'user')]):
-				self.user=data
-			elif self.stack[-1]==('span', [('class', 'meta')]):
-				self.meta=data
-			elif (self.desired_user and self.user==self.desired_user) or ((not self.desired_user) and self.user):
-				for word in data.split():
-					self.wordcount[re.sub(r'[^a-z\-\']', '', word.lower())]+=1
-
-	parser=MessageParser(args.word_frequency_user)
-	import os
-	with open(os.path.join(args.path, 'html', 'messages.htm')) as file: parser.feed(file.read())
+	parser=WordCountParser(args.word_frequency_user)
+	parser.go()
 	wordcount=list(parser.wordcount.items())
 	wordcount.sort(key=lambda x: x[1])
 	with open('wc.txt', 'w') as file:
